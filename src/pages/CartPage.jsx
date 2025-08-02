41b213744e237d7b01d1c60a5da78964c35b9d85
atiya-inayat <atiyainayat869@gmail.com>
2025-08-02 20:01:27 +0500

    adding increase and decrease qty , remove in cart

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -5,18 +5,21 @@ const CartPage = () => {
   const cartItems = useCartStore((state) => state.cartItems);
   const removeBtn = useCartStore((state) => state.removeBtn);
   const totalPrice = useCartStore((state) => state.totalPrice);
+  const clearCart = useCartStore((state) => state.clearCart);
 
   return (
     <>
       {cartItems.map((item) => (
         <div>
-          <div>
+          <div key={item.id}>
             <h1>{item.title}</h1>
-            <h2>${item.price}</h2>
+            <h2>Price: ${item.price}</h2>
             <p>Quantity: {item.quantity}</p>
+            <p>Subtotal: ${item.quantity * item.price}</p>
             <button onClick={() => removeBtn(item.id)}>Remove Item</button>
           </div>
           <h3>Total: ${totalPrice()}</h3>
+          <button onClick={clearCart}>Clear Cart</button>
         </div>
       ))}
     </>
--- src/store/cartStore.js
+++ src/store/cartStore.js
@@ -29,6 +29,31 @@ const useCartStore = create((set, get) => ({
     const cartItems = get().cartItems;
     return cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
   },
+  clearCart: () => {
+    const cartItems = get().cartItems;
+    if (window.confirm("Are you sure you want to clear cart")) {
+      set({ cartItems: [] });
+      console.log("item deleted...");
+    } else {
+      console.log("Deletion cancelled");
+    }
+  },
+  increaseQty: (id) => {
+    const cartItems = get().cartItems;
+    const inCrementedQty = cartItems.map((item) =>
+      item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+    );
+    set({ cartItems: inCrementedQty });
+  },
+  decreaseQty: (id) => {
+    const cartItems = get().cartItems;
+    const deCrementedQty = cartItems
+      .map((item) =>
+        item.id === id ? { ...item, quantity: item.quantity - 1 } : item
+      )
+      .filter((item) => item.quantity > 0); // remove item with 0 quantity
+    set({ cartItems: deCrementedQty });
+  },
 }));
 
 export default useCartStore;
